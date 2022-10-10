f8b973c35262be310bb56d5fc231a90ce233ed62
Najlekk <37276870+Najlekk@users.noreply.github.com>
2022-10-10 23:54:54 +0200

    Removed unused endpoint, added image-type to url

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,6 @@ const moment = require('moment');
 router.get('/:userId', function(req, res, next) {
   res.setHeader('content-type', 'application/json');
   const resp_data = {}
-  //console.log(req.params.userId.toString().length);
   if(req.params.userId.toString().length < 17){
     resp_data['error'] = "Invalid id";
     res.send(resp_data)
@@ -29,37 +28,11 @@ router.get('/:userId', function(req, res, next) {
     const unix = convertSnowflakeToUnix(resp_data['id']);
     const timestamp = moment.unix(unix/1000);
     resp_data['formatted_timestamp'] = timestamp.utc().format('MM-DD-YYYY, HH:mm:ss');
-    if(resp_data['avatar'] != null) resp_data['avatar_url'] = 'https://cdn.discordapp.com/avatars/' + resp_data['id'] + '/' + resp_data['avatar'];
-    if(resp_data['banner'] != null) resp_data['banner_url'] = 'https://cdn.discordapp.com/banners/' + resp_data['id'] + '/' + resp_data['banner'];
+    if(resp_data['avatar'] != null) resp_data['avatar_url'] = 'https://cdn.discordapp.com/avatars/' + resp_data['id'] + '/' + resp_data['avatar'] + (resp_data['avatar'].startsWith("a_") ? ".gif" : ".png");
+    if(resp_data['banner'] != null) resp_data['banner_url'] = 'https://cdn.discordapp.com/banners/' + resp_data['id'] + '/' + resp_data['banner'] + (resp_data['banner'].startsWith("a_") ? ".gif" : ".png");
     res.send(resp_data)
   });
 });
-
-/*
-router.get('/users/me', function(req, res, next) {
-  res.setHeader('content-type', 'application/json');
-  const resp_data = {}
-  axios.get('https://discord.com/api/users/@me', {
-    headers: {
-      'Authorization': settings.auth
-    }
-  }).then(response => {
-    if(response.status !== 200){
-      resp_data['error'] = "Invalid id";
-      res.send(resp_data)
-      return;
-    }
-    for(const key in response.data){
-      resp_data[key] = response.data[key];
-    }
-    const unix = convertSnowflakeToUnix(resp_data['id']);
-    const timestamp = moment.unix(unix/1000);
-    resp_data['formatted_timestamp'] = timestamp.utc().format('MM-DD-YYYY, HH:mm:ss');
-    if(resp_data['avatar'] != null) resp_data['avatar_url'] = 'https://cdn.discordapp.com/avatars/' + resp_data['id'] + '/' + resp_data['avatar'];
-    if(resp_data['banner'] != null) resp_data['banner_url'] = 'https://cdn.discordapp.com/banners/' + resp_data['id'] + '/' + resp_data['banner'];
-    res.send(resp_data)
-  });
-});*/
 
 //Convert discord id to unix
 function convertSnowflakeToUnix(snowflake) {
